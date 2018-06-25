9f07ef88b2718e96bb5bc816846c5c6266a0dd6b
tapan <tapankumarpatro05@gmail.com>
2018-06-25 15:14:19 +0530

    Creating new project with django with 2 app pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# rest_api_django_demo_try
-Trying out Python Framework Django for rest api development.
---
+++ django_demo/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tapan/PycharmProjects/django_demo/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
